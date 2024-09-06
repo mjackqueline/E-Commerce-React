4a7d6ff5e21d42b9843ef5012c31939d740919b9
Jackqueline <mjackqueline22@gmail.com>
2024-09-06 13:02:55 -0700

    edited function of bookExistsOnCart

--- src/Pages/BookInfo.jsx
+++ src/Pages/BookInfo.jsx
@@ -14,7 +14,7 @@ const BookInfo = ({ books, addToCart, cart }) => {
   }
 
   function bookExistsOnCart() {
-    return cart && cart.find((book) => +book.id === +id); 
+    return cart.find((book) => book.id === +id); 
   }
  
 
